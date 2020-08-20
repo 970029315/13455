7d63f824da3c595a60a74b44e899bc9bd4bb9f7c
“github’970029315git config --global user.email “g970029315@qq.com <970029315@qq.com>
2020-08-20 17:23:16 +0800

    主页功能完成

--- assets/js/bastAPI.js
+++ assets/js/bastAPI.js
@@ -1,3 +1,14 @@
 $.ajaxPrefilter(function(options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function(res) {
+        if (res.responseJSON.status !== 0 && res.responseJSON.message !== '获取用户基本信息成功！') {
+            localStorage.removeItem('token')
+            location.href = 'login.html'
+        }
+    }
 })
